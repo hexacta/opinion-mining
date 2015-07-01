745f7f0f2635d5bff64fc05955a0314832cb6b25
Christian Marcusa <chaitooler@gmail.com>
2015-07-01 15:59:31 -0300

    Vagrant setup for the app

---
+++ vagrant_setup.sh
@@ -0,0 +1,6 @@
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+sudo apt-get install curl
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /home/vagrant/.rvm/scripts/rvm
+rvm install ruby
+gem install twitter
