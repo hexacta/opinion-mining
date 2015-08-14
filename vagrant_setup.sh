24182a39457bf06991b444e1b7f9a2f07446a988
Manuel Pérez Vega <mperezvega@hexacta.com>
2015-08-14 12:27:59 -0300

    Cleanup old project

--- vagrant_setup.sh
+++
@@ -1,7 +0,0 @@
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-sudo apt-get install curl
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-source /home/vagrant/.rvm/scripts/rvm
-gem install twitter
-gem install bundler
-bundle install
