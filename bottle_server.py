9e903d0edcd832f75845e2f10283996ef5700467
Manuel Pérez Vega <mperezvega@hexacta.com>
2015-08-16 18:36:18 -0300

    Adding first REST service as example

--- bottle_server.py
+++ bottle_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from bottle import route, run, hook, response
+from json import dumps
 from core.sentiments_analyser import SentimentsAnalyser
 
 @hook('after_request')
@@ -27,5 +28,16 @@ def sentiments(text):
         "Result" : analysis
     }
     return result
+
+@route('/example')
+def example():
+    example = [
+        { "id": 1, "word": "Hexacta", "date": "2015-08-01", "positive": 33, "neutral": 13, "negative": 15 },
+        { "id": 2, "word": "Hexacta", "date": "2015-08-02", "positive": 14, "neutral": 10, "negative": 12 },
+        { "id": 3, "word": "Hexacta", "date": "2015-08-03", "positive": 25, "neutral": 4, "negative": 22 },
+    ]
+    response.content_type = 'application/json'
+    return dumps(example)
+
 
 run(host='0.0.0.0', port=9001, reloader=True)
