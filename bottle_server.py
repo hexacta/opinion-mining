13f5a6f20eb8909f69644a2f5a3dc61607319b04
Manuel Pérez Vega <mperezvega@hexacta.com>
2015-08-20 18:29:58 -0300

    json example

--- bottle_server.py
+++ bottle_server.py
@@ -1,43 +1,67 @@
 #!/usr/bin/env python
 
-from bottle import route, run, hook, response
 from json import dumps
+
+from bottle import route, run, hook, response
+
 from core.sentiments_analyser import SentimentsAnalyser
+
 
 @hook('after_request')
 def enable_cors():
     response.headers['Access-Control-Allow-Origin'] = '*'
 
+
 @route('/')
 def root():
     result = {
-        "USAGE" :
+        "USAGE":
             [
-                {"Usage and examples" : "/"},
-                {"Sentiments analysis" :"/sentiments/text"},
+                {"Usage and examples": "/"},
+                {"Sentiments analysis": "/sentiments/text"},
             ]
     }
     return result
+
 
 @route('/sentiments/<text>')
 def sentiments(text):
     analyser = SentimentsAnalyser()
     analysis = analyser.analyze(text)
     result = {
-        "Text"   : text,
-        "Result" : analysis
+        "Text": text,
+        "Result": analysis
     }
     return result
 
+
 @route('/example')
 def example():
-    example = [
-        { "id": 1, "word": "Hexacta", "date": "2015-08-01", "positive": 33, "neutral": 13, "negative": 15 },
-        { "id": 2, "word": "Hexacta", "date": "2015-08-02", "positive": 14, "neutral": 10, "negative": 12 },
-        { "id": 3, "word": "Hexacta", "date": "2015-08-03", "positive": 25, "neutral": 4, "negative": 22 },
-    ]
+    data = {
+        "word": "Hexacta",
+        "current_positive_percentage": 44,
+        "current_neutral_percentage": 32,
+        "current_negative_percentage": 24,
+        "history": {
+            "positve": [
+                {"date": "2015-08-01", "value": 55},
+                {"date": "2015-08-02", "value": 57},
+                {"date": "2015-08-03", "value": 64},
+            ],
+            "neutral": [
+                {"date": "2015-08-01", "value": 25},
+                {"date": "2015-08-02", "value": 23},
+                {"date": "2015-08-03", "value": 31},
+            ],
+            "negative": [
+                {"date": "2015-08-01", "value": 20},
+                {"date": "2015-08-02", "value": 10},
+                {"date": "2015-08-03", "value": 5},
+            ]
+        }
+    };
     response.content_type = 'application/json'
-    return dumps(example)
+    return dumps(data)
 
 
 run(host='0.0.0.0', port=9001, reloader=True)
